2325ff36f104a7a427496eeb92ecd69de4a87092
DevRahulDS <rahul.ds@ikure.in>
2024-11-10 22:18:27 +0530

    fix: remove any

--- src/components/contact-us/contact-us.tsx
+++ src/components/contact-us/contact-us.tsx
@@ -40,7 +40,7 @@ export function ContactForm() {
     },
   });
 
-  const onSubmit = (data: any) => {
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
     console.log(data); // handle form submission here
   };
 
